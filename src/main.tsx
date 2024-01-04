158f105fa67e1fa9bbc23bd33c3b9ece3d317535
Dasewasia <77565642+Dasewasia10@users.noreply.github.com>
2024-01-04 16:26:39 +0800

    add dist folder and delete main.css, back to dist/output.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.css';
+import '../dist/output.css';
 import App from './App';
 import { DarkModeProvider } from './context/DarkModeContext';
 
